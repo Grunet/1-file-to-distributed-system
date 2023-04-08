4af13dc0533cd5a098995fc203343fddf30177a7
Grunet <sadikshahidain@gmail.com>
2023-04-08 00:17:07 -0500

    Move service map to bottom

--- index.ts
+++ index.ts
@@ -1,22 +1,5 @@
 import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 
-const serviceMap = {
-  orders: {
-    envVarNames: {
-      startup: "START_ORDER_SERVICE",
-      hostname: "ORDER_SERVICE_HOSTNAME",
-    },
-    port: 8080,
-  },
-  inventory: {
-    envVarNames: {
-      startup: "START_INVENTORY_SERVICE",
-      hostname: "INVENTORY_SERVICE_HOSTNAME",
-    },
-    port: 8081,
-  },
-};
-
 const ordersHandler = async (request: Request): Promise<Response> => {
   await fetch(Deno.env.get(serviceMap.inventory.envVarNames.hostname) ?? "");
 
@@ -42,5 +25,22 @@ const inventoryHandler = (request: Request): Response => {
 if (Deno.env.get(serviceMap.orders.envVarNames.startup)) {
   await serve(inventoryHandler, { port: serviceMap.inventory.port });
 }
+
+const serviceMap = {
+  orders: {
+    envVarNames: {
+      startup: "START_ORDER_SERVICE",
+      hostname: "ORDER_SERVICE_HOSTNAME",
+    },
+    port: 8080,
+  },
+  inventory: {
+    envVarNames: {
+      startup: "START_INVENTORY_SERVICE",
+      hostname: "INVENTORY_SERVICE_HOSTNAME",
+    },
+    port: 8081,
+  },
+};
 
 export { serviceMap };
